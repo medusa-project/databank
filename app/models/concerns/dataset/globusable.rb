fbe91c755e1f8841517dd088de05c801683b94d8
Colleen Fallaw <mfall3@illinois.edu>
2020-01-24 11:01:25 -0600

    refine globus download

--- app/models/concerns/dataset/globusable.rb
+++ app/models/concerns/dataset/globusable.rb
@@ -4,10 +4,12 @@ module Globusable
   extend ActiveSupport::Concern
   def globus_downloadable?
     return false unless self.publication_state == Databank::PublicationState::RELEASED
+
     self.datafiles.each do |datafile|
       return false unless Application.storage_manager.globus_download_root.exist?("#{self.key}/#{datafile.binary_name}")
+
     end
-    return true
+    true
   end
   def globus_download_dir
     if Rails.env.demo?
