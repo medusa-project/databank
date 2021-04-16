ec164005e4c8c4c6c8529560c8069809d58615b9
Colleen Fallaw <mfall3@illinois.edu>
2021-04-16 14:47:07 -0500

    fix typo

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -461,14 +461,8 @@ class Datafile < ApplicationRecord
     Rails.logger.warn "inside handle_extractor_success"
     self.update(peek_type: message_obj["peek_type"], peek_text: message_obj["peek_text"])
     self.nested_items.destroy_all
-
-    # TEMPORARY DEBUG
-    Rails.logger.warn message_obj.keys
-    Rails.logger.warn message_obj["nested_itmes"]
     message_obj["nested_items"].each do |raw_item|
       item = JSON.parse(raw_item)
-      # TEMPORARY DEBUG
-      Rails.logger.warn "item: #{item}"
       NestedItem.create!(datafile_id:  self.id,
                         item_name:    item["item_name"],
                         item_path:    item["item_path"],
