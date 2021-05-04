09ae93c6c2f12220c1cce77b83830534e50d99b6
Colleen Fallaw <mfall3@illinois.edu>
2021-05-04 13:42:45 -0500

    refine sharing link interface

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -51,7 +51,7 @@ class DatasetsController < ApplicationController
 
   def share
     @dataset.create_share_code(id: @dataset.id) unless @dataset.current_share_code
-    share_notice = %Q(<h3>Private Sharing Link now available on this page below Citation</h3><ul><li>Anybody with this link can access to your private dataset
+    share_notice = %Q(<ul><li>Anybody with this link can access to your private dataset
 so be careful who you share it with.</li>
 <li>This link can be used at the journal office during the review process or for sharing with
 collaborators to access the data files while the dataset is not public.</li>
