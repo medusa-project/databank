d5490bad80f9997ae1390dfd1fb3cbff572ab869
Colleen Fallaw <mfall3@illinois.edu>
2022-11-16 08:30:46 -0600

    refine authorization error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   include CanCan::ControllerAdditions
 
   rescue_from ActionView, with: :render400
-  rescue_from CanCan::AccessDenied, with: :handle_denied
+  rescue_from StandardError::CanCan::AccessDenied, with: :handle_denied
   rescue_from ActiveRecord::RecordNotFound, with: :render404
   rescue_from ActionController::InvalidCrossOriginRequest, with: :render400
   rescue_from RSolr::Error::Http, with: :render400
