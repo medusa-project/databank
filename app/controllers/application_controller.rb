2b248eaf7a05d93f93c6839038c005c84fd37e83
Colleen Fallaw <mfall3@illinois.edu>
2022-11-18 11:18:04 -0600

    refine exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,16 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   include CanCan::ControllerAdditions
+
+  rescue_from ActionController::InvalidCrossOriginRequest, with: :render_400
   rescue_from StandardError, with: :error_occurred
+  rescue_from ActionView::MissingTemplate do |_exception|
+    render json: {}, status: :unprocessable_entity
+  end
+
   after_action :store_location
 
-  def render400
+  def render_400
     self.response_body = nil
     render(nothing: true, status: :bad_request)
   end
@@ -19,74 +25,80 @@ class ApplicationController < ActionController::Base
   def store_location
     return unless request.get?
 
-    # don't store ajax calls
-    session[:previous_url] = request.fullpath if request.path != "/login" && request.path != "/logout" && !request.xhr?
+    if request.path != "/login" &&
+      request.path != "/logout" &&
+      !request.xhr? # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
     session[:previous_url] = "/datasets/new" if request.path == "/welcome/deposit_login_modal"
   end
 
   def redirect_path
-    session[:previous_url] || main_app.root_url
-  end
-
-  def handle_denied(exception)
-    if exception.action == :create
-      if current_user && current_user.role == "no_deposit"
-        redirect_to redirect_path,
-                    alert: "ACCOUNT NOT ELIGIBLE TO DEPOSIT DATA.<br/>Faculty, staff, and graduate students are eligible to deposit data in Illinois Data Bank.<br/>Please <a href='/help'>contact the Research Data Service</a> if this determination is in error, or if you have any questions."
-      else
-        redirect_to "/welcome/deposit_login_modal"
-      end
-    else
-      respond_to do |format|
-        format.html {
-          redirect_to redirect_path,
-                      alert:  "You are not authorized to access the requested resource.",
-                      status: :forbidden
-        }
-        format.json { render nothing: true, status: :forbidden }
-        format.xml { render xml: {error: "unauthorized"}.to_xml, status: :forbidden }
-      end
-    end
-  end
-
-  def render404
-    respond_to do |format|
-      format.html { render "errors/error404", status: :not_found }
-      format.json { render nothing: true, status: :not_found }
-      format.all { render "errors/error404", status: :not_found }
-    end
+    session[:previous_url] || IDB_CONFIG[:root_url_text]
   end
 
   protected
 
   def error_occurred(exception)
+    if exception.instance_of?(RSolr::Error::Http)
+      respond_to do |format|
+        format.html { redirect_to redirect_path, status: :bad_request }
+        format.json { render nothing: true, status: :bad_request }
+        format.xml { render xml: {error: "bad request"}.to_xml, status: :bad_request }
+      end
+    elsif exception.instance_of?(ActionController::InvalidCrossOriginRequest)
+      respond_to do |format|
+        format.html { redirect_to IDB_CONFIG[:root_url_text], status: :unauthorized }
+        format.json { render nothing: true, status: :unauthorized }
+        format.xml { render xml: {error: "unauthorized"}.to_xml, status: :unauthorized }
+      end
+      elseif exception.instance_of?(CanCan::AccessDenied)
+      if exception.action == :create
+        if current_user && current_user.role == "no_deposit"
+          redirect_to redirect_path,
+                      alert: "ACCOUNT NOT ELIGIBLE TO DEPOSIT DATA.<br/>Faculty, staff, and graduate students are eligible to deposit data in Illinois Data Bank.<br/>Please <a href='/help'>contact the Research Data Service</a> if this determination is in error, or if you have any questions."
+        else
+          redirect_to "/welcome/deposit_login_modal"
+        end
+      else
+        respond_to do |format|
+          format.html {
+            redirect_to redirect_path,
+                        alert:  "You are not authorized to access the requested resource.",
+                        status: :forbidden
+          }
+          format.json { render nothing: true, status: :forbidden }
+          format.xml { render xml: {error: "unauthorized"}.to_xml, status: :forbidden }
+        end
+      end
 
-    Rails.logger.warn exception.class
-    Rails.logger.warn exception.class.class
+    elsif exception.instance_of?(ActiveRecord::RecordNotFound)
+      respond_to do |format|
+        format.html { render "errors/error404", status: :not_found }
+        format.json { render nothing: true, status: :not_found }
+        format.all { render "errors/error404", status: :not_found }
+      end
 
-    case exception.class
-
-    when "CanCan::AccessDenied"
-      Rails.logger.warn ("inside CanCan::AccessDenied")
-      redirect_to 'handle_denied', exception: exception
-    when "ActiveRecord::RecordNotFound"
-      redirect_to 'render404'
-    when ["ActionView::MissingTemplate", "ActionController::InvalidCrossOriginRequest", "RSolr::Error::Http"]
-      redirect_to 'render400'
     else
-      Rails.logger.warn("inside else")
-      exception_string = "*** Standard Error caught in application_controller.rb on #{IDB_CONFIG[:root_url_text]} ***\nclass: #{exception.class}\nmessage: #{exception.message}\n"
-      exception_string << Time.now.utc.iso8601
+      exception_string_array = []
+      exception_string_array << "*** Standard Error caught in application_controller.rb on #{IDB_CONFIG[:root_url_text]} ***\nclass: #{exception.class}\nmessage: #{exception.message}\n"
+      exception_string_array << Time.now.utc.iso8601
 
-      exception_string << "\nstack:\n"
+      exception_string_array << "\nstack:\n"
       exception.backtrace.each do |line|
-        exception_string << line
-        exception_string << "\n"
+        exception_string_array << line
+        exception_string_array << "\n"
       end
 
       Rails.logger.warn(exception_string)
 
-      exception_string << "\nCurrent User: #{current_user.name} | #{current_user.email}" if current_user
+      exception_string_array << if current_user
+                                  "\nCurrent User: #{current_user.name}"
+                                else
+                                  current_user.email.to_s
+                                end
+
+      exception_string = exception_string_array.join("")
 
       notification = DatabankMailer.error(exception_string)
       notification.deliver_now
@@ -95,6 +107,7 @@ class ApplicationController < ActionController::Base
         format.json { render nothing: true, status: :internal_server_error }
         format.xml { render xml: {status: 500}.to_xml }
       end
+
     end
   end
 
@@ -130,9 +143,9 @@ class ApplicationController < ActionController::Base
   end
 
   def require_logged_in
-    return if logged_in?
-
-    session[:login_return_uri] = request.env["REQUEST_URI"]
-    redirect_to(login_path)
+    unless logged_in?
+      session[:login_return_uri] = request.env["REQUEST_URI"]
+      redirect_to(login_path)
+    end
   end
 end
