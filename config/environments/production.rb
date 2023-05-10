42a3d20551eeb87f4b9b874e83c189a94cedb4f0
Colleen Fallaw <mfall3@illinois.edu>
2023-05-10 09:50:00 -0500

    refine updates to Google-targeted config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #Google Analtyics Code
-  GA.tracker = "UA-74879540-1"
+  #GA.tracker = "UA-74879540-1"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
