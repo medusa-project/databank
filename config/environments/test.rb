f8fe56eaf367fe00b9e3ac165c17e2df8f11c9e9
Colleen Fallaw <mfall3@illinois.edu>
2023-06-16 14:47:07 -0500

    remove encryption for development and test configurations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  config.require_master_key = true
+  config.require_master_key = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
--- config/initializers/amqp.rb
+++ config/initializers/amqp.rb
@@ -1,4 +1,12 @@
-amqp_settings_path = File.join(Rails.root, 'config', 'amqp.yml')
-amqp_settings = YAML.load(ERB.new(File.read(amqp_settings_path)).result)[Rails.env]
+if Rails.env.development?
+  amqp_settings_path = File.join(Rails.root, 'config', 'amqp-development.yml')
+  amqp_settings = YAML.load(ERB.new(File.read(amqp_settings_path)).result)
+elsif Rails.env.test?
+  amqp_settings_path = File.join(Rails.root, 'config', 'amqp-test.yml')
+  amqp_settings = YAML.load(ERB.new(File.read(amqp_settings_path)).result)
+elsif Rails.env.demo? || Rails.env.production?
+  amqp_settings_path = File.join(Rails.root, 'config', 'amqp.yml')
+  amqp_settings = YAML.load(ERB.new(File.read(amqp_settings_path)).result)[Rails.env]
+end
 
 AmqpHelper::Connector.new(:databank, amqp_settings)
