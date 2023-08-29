3dc13e11b85fbd61665a99ae05a4685254a4fdcc
Colleen Fallaw <mfall3@illinois.edu>
2023-08-29 16:24:18 -0500

    refine version request workflow

--- config/routes.rb
+++ config/routes.rb
@@ -101,6 +101,7 @@ Rails.application.routes.draw do
     member do
       get 'confirm_review', to: 'datasets#confirm_review'
       match 'send_publication_notice', to: 'datasets#send_publication_notice', via: [:get, :post]
+      match 'version_confirm', via: [:get, :post, :patch]
       get 'share'
       get 'version', to: 'datasets#pre_version'
       get 'version_confirm'
