1edc4b6394419fd0ba2778fdf0993cd17d15b7fe
Colleen Fallaw <mfall3@illinois.edu>
2024-01-29 11:23:34 -0600

    merge rocky environment updates

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
-IDB_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, "config/databank-ci.yml"))).result)
-STORAGE_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, "config/medusa-storage-ci.yml"))).result)
+IDB_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, "config/databank-test.yml"))).result)
+STORAGE_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, "config/medusa-storage-test.yml"))).result)
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -7,10 +7,8 @@ STORAGE_CONFIG = YAML.load(ERB.new(File.read(File.join(Rails.root, "config/medus
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
-
-  config.log_level = :warn
-
   # Settings specified here will take precedence over those in config/application.rb.
+
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
