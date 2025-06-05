ead64360a5360dbf3c353868859e7cff663fa2d4
Colleen Fallaw <mfall3@illinois.edu>
2025-06-05 16:59:05 -0500

    clarify self context for method calls in Metric

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -21,25 +21,25 @@ class Metric
     ##
     # @return [Hash] the modified times of the metrics
     def modified_times
-      write_dataset_downloads_json unless File.exist?(METRICS_CONFIG[:dataset_downloads_json][:relative_path])
+      self.write_dataset_downloads_json unless File.exist?(METRICS_CONFIG[:dataset_downloads_json][:relative_path])
       raise StandardError.new("unable to create dataset downloads json") unless File.exist?(METRICS_CONFIG[:dataset_downloads_json][:relative_path])
 
-      write_datafile_downloads_json unless File.exist?(METRICS_CONFIG[:datafile_downloads_json][:relative_path])
+      self.write_datafile_downloads_json unless File.exist?(METRICS_CONFIG[:datafile_downloads_json][:relative_path])
       raise StandardError.new("unable to create datafile downloads json") unless File.exist?(METRICS_CONFIG[:datafile_downloads_json][:relative_path])
 
-      write_datafiles_csv unless File.exist?(METRICS_CONFIG[:datafiles_csv][:relative_path])
+      self.write_datafiles_csv unless File.exist?(METRICS_CONFIG[:datafiles_csv][:relative_path])
       raise StandardError.new("unable to create datafiles csv") unless File.exist?(METRICS_CONFIG[:datafiles_csv][:relative_path])
 
-      write_datasets_tsv unless File.exist?(METRICS_CONFIG[:datasets_tsv][:relative_path])
+      self.write_datasets_tsv unless File.exist?(METRICS_CONFIG[:datasets_tsv][:relative_path])
       raise StandardError.new("unable to create datasets tsv") unless File.exist?(METRICS_CONFIG[:datasets_tsv][:relative_path])
 
-      write_container_contents_csv unless File.exist?(METRICS_CONFIG[:container_contents_csv][:relative_path])
+      self.write_container_contents_csv unless File.exist?(METRICS_CONFIG[:container_contents_csv][:relative_path])
       raise StandardError.new("unable to create container contents csv") unless File.exist?(METRICS_CONFIG[:container_contents_csv][:relative_path])
 
-      write_funders_csv unless File.exist?(METRICS_CONFIG[:funders_csv][:relative_path])
+      self.write_funders_csv unless File.exist?(METRICS_CONFIG[:funders_csv][:relative_path])
       raise StandardError.new("unable to create funders csv") unless File.exist?(METRICS_CONFIG[:funders_csv][:relative_path])
 
-      write_related_materials_csv unless File.exist?(METRICS_CONFIG[:related_materials_csv][:relative_path])
+      self.write_related_materials_csv unless File.exist?(METRICS_CONFIG[:related_materials_csv][:relative_path])
       raise StandardError.new("unable to create related materials csv") unless File.exist?(METRICS_CONFIG[:related_materials_csv][:relative_path])
       # wait a second to ensure the files are written
       # This is necessary because the file system may not update the modified time immediately
