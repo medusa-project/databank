d4088fd4a922108daa458ee4151a4fc75b488187
Colleen Fallaw <mfall3@illinois.edu>
2025-10-31 09:07:38 -0500

    update trap focus in orcid search modal to permit import

--- app/assets/javascripts/creators.js
+++ app/assets/javascripts/creators.js
@@ -347,27 +347,7 @@ function enableOrcidImport() {
   jQuery('#orcid-import-btn').prop('disabled', false);
 }
 
-function trapFocus(e) {
-  const focusableSelectors = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex]:not([tabindex="-1"]), [contenteditable]';
-  const modal = e.currentTarget || e.target;
-  const focusableElements = modal.querySelectorAll(focusableSelectors);
-  const firstFocusable = focusableElements[0];
-  const lastFocusable = focusableElements[focusableElements.length - 1];
-  if (e.key === 'Tab') {
-    if (e.shiftKey) {
-      if (document.activeElement === firstFocusable) {
-        e.preventDefault();
-        lastFocusable.focus();
-      }
-    } else {
-      if (document.activeElement === lastFocusable) {
-        e.preventDefault();
-        firstFocusable.focus();
-      }
-    }
-  }
-}
-
+// Depends on a function trapFocus(e) defined in afirst.js
 function showCreatorOrcidSearchModal(creator_index) {
   jQuery('#orcid-import-btn').prop('disabled', true);
   jQuery("#creator-index").val(creator_index);
