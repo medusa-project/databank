b5d251edbcd848b7104b8c2a2ba7f2670d443c2b
mfall3 <mfall3@illinois.edu>
2026-05-14 16:46:44 -0500

    add runtime saftey patterns, extract some duplicated functionality

--- app/assets/javascripts/files_preview_ui.js
+++ app/assets/javascripts/files_preview_ui.js
@@ -1,70 +1,98 @@
 (function (window) {
-    window.Databank = window.Databank || {};
-    Databank.files = Databank.files || {};
+  window.Databank = window.Databank || {};
+  Databank.files = Databank.files || {};
 
-    Databank.files.previewUI = {
-        preview: function (web_id) {
-            jQuery('#preview_' + web_id).show();
+  Databank.files.previewUI = {
+    preview: function (web_id) {
+      jQuery("#preview_" + web_id).show();
 
-            if (jQuery('#preview_' + web_id).hasClass('fetched')) {
-                console.log('using previously fetched text');
-            } else {
-                jQuery('.spinner_' + web_id).show();
+      if (jQuery("#preview_" + web_id).hasClass("fetched")) {
+        console.log("using previously fetched text");
+      } else {
+        jQuery(".spinner_" + web_id).show();
 
-                jQuery.getJSON('/datafiles/' + web_id + '/viewtext', function (json) {
-                    jQuery('#preview_' + web_id).html('<pre>' + json.peek_text + '</pre>');
-                    jQuery('#preview_' + web_id).addClass('fetched');
-                    jQuery('.spinner_' + web_id).hide();
-                });
-            }
+        jQuery.getJSON("/datafiles/" + web_id + "/viewtext", function (json) {
+          jQuery("#preview_" + web_id).html(
+            "<pre>" + json.peek_text + "</pre>",
+          );
+          jQuery("#preview_" + web_id).addClass("fetched");
+          jQuery(".spinner_" + web_id).hide();
+        });
+      }
 
-            jQuery('#preview_btn_' + web_id).attr('aria-expanded', 'true');
-            jQuery('#preview_glyph_' + web_id).removeClass('glyphicon-eye-open');
-            jQuery('#preview_glyph_' + web_id).addClass('glyphicon-eye-close');
-            jQuery('#preview_btn_' + web_id).attr('onclick', "hide_preview('" + web_id + "')");
-        },
+      jQuery("#preview_btn_" + web_id).attr("aria-expanded", "true");
+      jQuery("#preview_glyph_" + web_id).removeClass("glyphicon-eye-open");
+      jQuery("#preview_glyph_" + web_id).addClass("glyphicon-eye-close");
+      jQuery("#preview_btn_" + web_id).attr(
+        "onclick",
+        "hide_preview('" + web_id + "')",
+      );
+    },
 
-        previewMd: function (web_id) {
-            jQuery('#preview_' + web_id).show();
-            jQuery('#preview_btn_' + web_id).attr('aria-expanded', 'true');
-            jQuery('#preview_glyph_' + web_id).removeClass('glyphicon-eye-open');
-            jQuery('#preview_glyph_' + web_id).addClass('glyphicon-eye-close');
-            jQuery('#preview_md_btn_' + web_id).attr('onclick', "hide_md_preview('" + web_id + "')");
-        },
+    previewMd: function (web_id) {
+      jQuery("#preview_" + web_id).show();
+      jQuery("#preview_btn_" + web_id).attr("aria-expanded", "true");
+      jQuery("#preview_glyph_" + web_id).removeClass("glyphicon-eye-open");
+      jQuery("#preview_glyph_" + web_id).addClass("glyphicon-eye-close");
+      jQuery("#preview_md_btn_" + web_id).attr(
+        "onclick",
+        "hide_md_preview('" + web_id + "')",
+      );
+    },
 
-        hideMdPreview: function (web_id) {
-            jQuery('#preview_btn_' + web_id).attr('aria-expanded', 'falase');
-            jQuery('#preview_glyph_' + web_id).removeClass('glyphicon-eye-close');
-            jQuery('#preview_glyph_' + web_id).addClass('glyphicon-eye-open');
-            jQuery('#preview_md_btn_' + web_id).attr('onclick', "preview_md('" + web_id + "')");
-            jQuery('#preview_' + web_id).hide();
-        },
+    hideMdPreview: function (web_id) {
+      jQuery("#preview_btn_" + web_id).attr("aria-expanded", "false");
+      jQuery("#preview_glyph_" + web_id).removeClass("glyphicon-eye-close");
+      jQuery("#preview_glyph_" + web_id).addClass("glyphicon-eye-open");
+      jQuery("#preview_md_btn_" + web_id).attr(
+        "onclick",
+        "preview_md('" + web_id + "')",
+      );
+      jQuery("#preview_" + web_id).hide();
+    },
 
-        hidePreview: function (web_id) {
-            jQuery('#preview_btn_' + web_id).attr('aria-expanded', 'false');
-            jQuery('#preview_glyph_' + web_id).removeClass('glyphicon-eye-close');
-            jQuery('#preview_glyph_' + web_id).addClass('glyphicon-eye-open');
-            jQuery('#preview_btn_' + web_id).attr('onclick', "preview('" + web_id + "')");
-            jQuery('#preview_' + web_id).hide();
-        },
+    hidePreview: function (web_id) {
+      jQuery("#preview_btn_" + web_id).attr("aria-expanded", "false");
+      jQuery("#preview_glyph_" + web_id).removeClass("glyphicon-eye-close");
+      jQuery("#preview_glyph_" + web_id).addClass("glyphicon-eye-open");
+      jQuery("#preview_btn_" + web_id).attr(
+        "onclick",
+        "preview('" + web_id + "')",
+      );
+      jQuery("#preview_" + web_id).hide();
+    },
 
-        previewImage: function (iiif_root, web_id) {
-            jQuery('#preview_' + web_id).show();
-            if (jQuery('#preview_' + web_id).hasClass('fetched')) {
-                console.log('using previously fetched image');
-            } else {
-                jQuery('.spinner_' + web_id).show();
-                var image_url = iiif_root + '/' + web_id + '/full/max/0/default.jpg';
-                jQuery('#preview_' + web_id).addClass('fetched');
-                jQuery('#preview_' + web_id).html("<img src=" + image_url + " class='preview_body'>");
-                jQuery('.spinner_' + web_id).hide();
-            }
-            jQuery('#preview_img_btn_' + web_id).html('<button type="button" aria-expanded="true" class="btn btn-sm btn-success" onclick="hide_image_preview(&#39;' + iiif_root + '&#39;, &#39;' + web_id + '&#39;)"><span class="glyphicon glyphicon-eye-close"></span> View</button>');
-        },
+    previewImage: function (iiif_root, web_id) {
+      jQuery("#preview_" + web_id).show();
+      if (jQuery("#preview_" + web_id).hasClass("fetched")) {
+        console.log("using previously fetched image");
+      } else {
+        jQuery(".spinner_" + web_id).show();
+        var image_url = iiif_root + "/" + web_id + "/full/max/0/default.jpg";
+        jQuery("#preview_" + web_id).addClass("fetched");
+        jQuery("#preview_" + web_id).html(
+          "<img src=" + image_url + " class='preview_body'>",
+        );
+        jQuery(".spinner_" + web_id).hide();
+      }
+      jQuery("#preview_img_btn_" + web_id).html(
+        '<button type="button" aria-expanded="true" class="btn btn-sm btn-success" onclick="hide_image_preview(&#39;' +
+          iiif_root +
+          "&#39;, &#39;" +
+          web_id +
+          '&#39;)"><span class="glyphicon glyphicon-eye-close"></span> View</button>',
+      );
+    },
 
-        hideImagePreview: function (iiif_root, web_id) {
-            jQuery('#preview_img_btn_' + web_id).html('<button type="button" aria-expaneded="false" class="btn btn-sm btn-success" onclick="preview_image(&#39;' + iiif_root + '&#39;, &#39;' + web_id + '&#39;)"><span class="glyphicon glyphicon-eye-open"></span> View</button>');
-            jQuery('#preview_' + web_id).hide();
-        }
-    };
+    hideImagePreview: function (iiif_root, web_id) {
+      jQuery("#preview_img_btn_" + web_id).html(
+        '<button type="button" aria-expanded="false" class="btn btn-sm btn-success" onclick="preview_image(&#39;' +
+          iiif_root +
+          "&#39;, &#39;" +
+          web_id +
+          '&#39;)"><span class="glyphicon glyphicon-eye-open"></span> View</button>',
+      );
+      jQuery("#preview_" + web_id).hide();
+    },
+  };
 })(window);
--- app/assets/javascripts/idb_utils.js
+++ app/assets/javascripts/idb_utils.js
@@ -3,65 +3,110 @@
 // Legacy global function names remain available in their original files via thin wrappers.
 
 (function (window) {
-    window.Databank = window.Databank || {};
+  window.Databank = window.Databank || {};
 
-    window.Databank.utils = {
+  window.Databank.utils = {
+    // Email validation -------------------------------------------------
 
-        // Email validation -------------------------------------------------
+    // Returns true if the given string matches a basic e-mail pattern.
+    isEmail: function (email) {
+      var regex =
+        /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
+    },
 
-        // Returns true if the given string matches a basic e-mail pattern.
-        isEmail: function (email) {
-            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            return regex.test(email);
+    // Validates an e-mail <input> element and applies error styling.
+    // Keeps the caller-facing behaviour identical to the previous per-file
+    // handle_creator_email_change / handle_contributor_email_change functions.
+    validateEmailField: function (input) {
+      var $input = jQuery(input);
+      var value = $input.val();
+
+      if (Databank.utils.isEmail(value)) {
+        $input.closest("td").removeClass("input-field-required");
+        $input.removeClass("invalid-email");
+      } else if (value !== "") {
+        $input.addClass("invalid-email");
+        alert("email address must be in valid format");
+        $input.focus();
+      } else {
+        $input.removeClass("invalid-email");
+      }
+    },
+
+    // ORCID helpers ----------------------------------------------------
+
+    // Enables the "Import Selected ORCID" button inside the search modal.
+    // Duplicated verbatim in creators.js and contributors.js previously.
+    enableOrcidImport: function () {
+      jQuery("#orcid-import-btn").prop("disabled", false);
+    },
+
+    // Dynamic table helpers --------------------------------------------
+
+    // Returns a jQuery UI sortable options object with the standard
+    // drag-handle and highlight behaviour used by all dynamic tables.
+    // Pass an `onUpdate` function as the sortable `update` callback.
+    sortableTableOptions: function (onUpdate) {
+      return {
+        axis: "y",
+        items: ".item",
+        cursor: "move",
+        sort: function (e, ui) {
+          return ui.item.addClass("active-item-shadow");
         },
-
-        // Validates an e-mail <input> element and applies error styling.
-        // Keeps the caller-facing behaviour identical to the previous per-file
-        // handle_creator_email_change / handle_contributor_email_change functions.
-        validateEmailField: function (input) {
-            var $input = jQuery(input);
-            var value = $input.val();
-
-            if (Databank.utils.isEmail(value)) {
-                $input.closest('td').removeClass('input-field-required');
-                $input.removeClass('invalid-email');
-            } else if (value !== '') {
-                $input.addClass('invalid-email');
-                alert('email address must be in valid format');
-                $input.focus();
-            } else {
-                $input.removeClass('invalid-email');
-            }
+        stop: function (e, ui) {
+          ui.item.removeClass("active-item-shadow");
+          return ui.item.children("td").effect("highlight", {}, 1000);
         },
+        update: onUpdate || function () {},
+      };
+    },
 
-        // ORCID helpers ----------------------------------------------------
+    // Builds standard Remove/Add action button markup for dynamic rows.
+    // Supports either inline onclick handlers or data-attribute actions.
+    rowActionButtons: function (options) {
+      var removeButton =
+        '<button class="' +
+        (options.removeClass || "btn btn-danger btn-sm") +
+        '" ' +
+        Databank.utils.buildHtmlAttributes(options.removeAttributes) +
+        ' type="button">' +
+        (options.removeLabel || "Remove") +
+        "</button>";
 
-        // Enables the "Import Selected ORCID" button inside the search modal.
-        // Duplicated verbatim in creators.js and contributors.js previously.
-        enableOrcidImport: function () {
-            jQuery('#orcid-import-btn').prop('disabled', false);
-        },
+      if (!options.includeAdd) {
+        return removeButton;
+      }
 
-        // Dynamic table helpers --------------------------------------------
+      var addButton =
+        '<button class="' +
+        (options.addClass || "btn btn-success btn-sm") +
+        '" ' +
+        Databank.utils.buildHtmlAttributes(options.addAttributes) +
+        ' type="button">' +
+        (options.addLabel || "Add") +
+        "</button>";
 
-        // Returns a jQuery UI sortable options object with the standard
-        // drag-handle and highlight behaviour used by all dynamic tables.
-        // Pass an `onUpdate` function as the sortable `update` callback.
-        sortableTableOptions: function (onUpdate) {
-            return {
-                axis: 'y',
-                items: '.item',
-                cursor: 'move',
-                sort: function (e, ui) {
-                    return ui.item.addClass('active-item-shadow');
-                },
-                stop: function (e, ui) {
-                    ui.item.removeClass('active-item-shadow');
-                    return ui.item.children('td').effect('highlight', {}, 1000);
-                },
-                update: onUpdate || function () {}
-            };
+      return removeButton + "&nbsp;&nbsp;" + addButton;
+    },
+
+    // Converts an object of attributes to an HTML attribute string.
+    buildHtmlAttributes: function (attributes) {
+      var attrString = "";
+      var key;
+
+      if (!attributes) {
+        return attrString;
+      }
+
+      for (key in attributes) {
+        if (Object.prototype.hasOwnProperty.call(attributes, key)) {
+          attrString += key + '="' + attributes[key] + '" ';
         }
-    };
+      }
 
+      return attrString;
+    },
+  };
 })(window);
